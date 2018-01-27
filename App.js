3c21a983812fe7a4c7b2c5f5eaa1fdeb69da325b
Santiago Gutierrez <ssgutierrez42@gmail.com>
2018-01-27 00:10:44 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,77 @@
+/*
+*
+* Assignment 3
+* Starter Files
+*
+* CS47SI
+* Jan, 2017
+*/
+
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Images, Colors } from './App/Themes'
+import APIRequest from './App/Config/APIRequest'
+
+import NavigationButtons from './App/Components/NavigationButtons'
+import News from './App/Components/News'
+import Search from './App/Components/Search'
+
+export default class App extends React.Component {
+
+  state = {
+    loading: true,
+    articles : [],
+    searchText: "",
+    category: ''
+  }
+
+  componentDidMount() {
+
+    //uncomment this to run an API query!
+    //this.loadArticles();
+  }
+
+  async loadArticles(searchTerm = '', category = '') {
+    this.setState({articles:[], loading: true});
+    var resultArticles = [];
+    if (category === '') {
+      resultArticles = await APIRequest.requestSearchPosts(searchTerm);
+    } else {
+      var resultArticles = await APIRequest.requestCategoryPosts(category);
+    }
+    this.setState({loading: false, articles: resultArticles})
+  }
+
+  render() {
+    const {articles, loading} = this.state;
+
+    return (
+      <SafeAreaView style={styles.container}>
+
+        <Text style={{textAlign: 'center'}}>Have fun! Start by taking a look at the NavigationButtons, Search, and News components.</Text>
+
+        {/*First, you'll need a logo*/}
+
+        {/*Then your "navigation" vector icons*/}
+
+        {/*A search bar*/}
+
+        {/*And some news*/}
+
+        {/*Though, you can style and organize these however you want! power to you 😎*/}
+
+        {/*If you want to return custom stuff from the NYT API, checkout the APIRequest file!*/}
+
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
